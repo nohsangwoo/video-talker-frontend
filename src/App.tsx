4808a483bbb2e04168e224c257911d0cbc520d58
nohsangwoo <nsgr12@naver.com>
2021-07-07 17:59:15 +0900

    apply router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React, { useEffect } from 'react';
-
 import './App.css';
 import connectWithWebSocket from './utils/wssConnection/wssConnection';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './LoginPage/LoginPage';
 
 function App() {
   useEffect(() => {
     connectWithWebSocket();
   }, []);
-  return <div className="App">hi its home</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
